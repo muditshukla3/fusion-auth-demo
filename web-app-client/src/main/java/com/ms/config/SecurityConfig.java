cda5ceee3434be5336a89f60507704f33ea104fe
muditshukla3 <muditshukla3@gmail.com>
2024-12-03 13:07:38 +0530

    Updated spring boot version.

--- web-app-client/src/main/java/com/ms/config/SecurityConfig.java
+++ web-app-client/src/main/java/com/ms/config/SecurityConfig.java
@@ -5,31 +5,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, ClientRegistrationRepository repo) throws Exception {
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
-        http
-                .authorizeRequests(a -> a
-                        .antMatchers("/", "/images/**", "/css/**","/initiate-logout")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated())
-                        .oauth2Login(Customizer.withDefaults());
-        http.logout(logout -> logout
-                .logoutUrl("/initiate-logout")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessHandler(logoutSuccessHandler())
-                .permitAll()
-        );
+    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.
+                authorizeHttpRequests(auth ->
+                        auth.
+                                requestMatchers("/","/images/**","/css/**", "/public/**").
+                                permitAll().
+                                anyRequest().
+                                authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/initiate-logout")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessHandler(logoutSuccessHandler())
+                        .permitAll());
 
         return http.build();
     }
--- web-app-client/src/main/java/com/ms/controller/CustomLogoutHandler.java
+++ web-app-client/src/main/java/com/ms/controller/CustomLogoutHandler.java
@@ -1,12 +1,12 @@
 package com.ms.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomLogoutHandler implements LogoutSuccessHandler {
